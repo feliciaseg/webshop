8f7b4ca48e15c45b29448a2604bda699c798cddd
jenerlov <jenerlo@gmail.com>
2021-03-11 13:54:21 +0100

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./styling/colorTheme";
 import { BrowserRouter as Router } from "react-router-dom";
 import ViewContainer from "./routes/ViewContainer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
+        <Header/>
         <ViewContainer />
       </Router>
     </ThemeProvider>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,25 @@
-export {}
+import { AppBar, Toolbar } from "@material-ui/core";
+import { CSSProperties } from "@material-ui/styles";
+import { Link } from "react-router-dom";
+
+
+
+
+
+export default function Header() {
+    return (
+
+        <header style={headerStyling}>
+         <AppBar position="fixed">
+            <Toolbar>
+            <Link to={{pathname: "./StartPage"}}><h1>Hem</h1></Link>
+            </Toolbar>
+        </AppBar>
+
+
+        </header>
+    )
+}
+
+const headerStyling: CSSProperties = {
+}
