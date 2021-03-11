852eda9b4a2205b090e431c6085ad3528ea6bed7
jenerlov <jenerlo@gmail.com>
2021-03-11 16:14:03 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,51 @@
-import { AppBar, Toolbar } from "@material-ui/core";
 import { CSSProperties } from "@material-ui/styles";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { theme } from "../styling/colorTheme";
+
+interface Props {
+    type: "transparent" | "white",
+
+}
+
+export default function Header(props: Props) {
+   let bgColor: string;
+   let textColor: string;
+
+    if (props.type === "transparent") {
+         bgColor = "transparent" 
+         textColor = "disabled"
+    } else {
+      bgColor = "white"
+      textColor = theme.palette.primary.main
+    }
 
 
-
-
-
-export default function Header() {
     return (
+         <div style={{...headerStyling, backgroundColor: bgColor}}>
+      
+                <Link style={{textDecoration: 'none'}}to={{pathname: './'}}><h1 style={h1}>HEMMA</h1></Link>
+                <Link style={{textDecoration: 'none'}}to={{pathname: './checkout' }}> 
+                <ShoppingCartIcon type="textColor" />
+               </Link>
 
-        <header style={headerStyling}>
-         <AppBar position="fixed">
-            <Toolbar>
-            <Link to={{pathname: "./StartPage"}}><h1>Hem</h1></Link>
-            </Toolbar>
-        </AppBar>
-
-
-        </header>
+     </div>
     )
 }
 
+
 const headerStyling: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '7rem',
+    width: '100%', 
+
+
 }
+
+const h1: CSSProperties = {
+    color: 'black',
+}
+
