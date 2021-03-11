ad9803200c6e62711df88fa1796ffb23c031c031
jenerlov <jenerlo@gmail.com>
2021-03-11 11:36:13 +0100

    lampor

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -3,6 +3,7 @@
 
 
 interface Product {
+    imageUrl: string,
     id: string,
     name: string,
     price: number,
@@ -10,10 +11,40 @@ interface Product {
 }
 
 export const products: Product[] = [
-    {
-        id: "",
-        name: "",
-        price: 0,
-        description: ""
-    }
+    {   
+        imageUrl: "https://assets.ellosgroup.com/i/ellos/b?$eg$&$emr$&$ep$&$ed$&n=ell_1520526_De_01&mw=750&rw=750&qlt=80",
+        id: "1520526",
+        name: "Fondi golvlampa",
+        price: 949,
+        description: "Fondi är en enkel och stilren golvlampa i svart metall. Ljushållaren i matt mässing viker av från den raka foten och riktar ljuset nedåt. "
+    },
+    {   
+        imageUrl: "https://assets.ellosgroup.com/i/ellos/b?$eg$&$emr$&$ep$&$ed$&n=ell_1536808-01_Fm_M0022614&mw=750&rw=750&qlt=80",
+        id: "1536808-01-0",
+        name: " Golvlampa Zac",
+        price: 1799,
+        description: "Golvlampa i pulverlackad metall med hängande kupa i transparent glas, diameter 18 cm. Höjden/positionen på kupan kan justeras genom att dra sladden upp eller ned."
+    },
+    {   
+        imageUrl: "https://assets.ellosgroup.com/i/ellos/b?$eg$&$emr$&$ep$&$ed$&n=ell_1596396-01_Fm_M0036602&mw=750&rw=750&qlt=80",
+        id: "1596396-01-0",
+        name: "Taklampa Mipal",
+        price: 999,
+        description: "Taklampa med skärm av konstrotting och stomme av målad metall. Skärmens mått: Diameter 40 cm, höjd 32 cm. Svart sladd av plast, sladdlängd 120 cm. Takkopp av metall med krokupphängning. Sockel E27. Max 40W."
+    },
+    {   
+        imageUrl: "https://assets.ellosgroup.com/i/ellos/b?$eg$&$emr$&$ep$&$ed$&n=ell_1594156-01_Fm_M0036734&mw=750&rw=750&qlt=80",
+        id: "1594156-01-0",
+        name: "Bordslampa Dorsia",
+        price: 999,
+        description: "Bordslampa i blank metall. Total höjd 40 cm. Mått på skärmen: Diameter 24 cm, höjd 10 cm. Svart sladd med strömbrytare, sladdlängd 1,5 m. Sockel E14. Max 40W."
+    },
+    {   
+        imageUrl: "https://assets.ellosgroup.com/i/ellos/b?$eg$&$emr$&$ep$&$ed$&n=ell_1597180-02_Fm_M0036733&mw=750&rw=750&qlt=80",
+        id: "1597180-02-0",
+        name: "Bordslampa Mushroom",
+        price: 1099,
+        description: "Bordslampa tillverkad av glas med liten extern detalj i mässingfinish (pläterad). Diameter 22 cm, höjd 30 cm. Transparent kabel med strömbrytare på sladden. Sladdlängd ca 195 cm. Sockel E14. Max 25W."
+    },
+
 ]
