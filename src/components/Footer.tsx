f1885382e627959feeb41d38627c6717d6f68a59
feliciaseg <felicia.segerholm97@gmail.com>
2021-03-11 19:38:35 +0100

    fully responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,38 @@
-import { Button} from "@material-ui/core";
+import { Button, StylesProvider } from "@material-ui/core";
 import React, { CSSProperties } from "react";
 import { Link } from "react-router-dom";
 import { theme } from "../styling/colorTheme";
 import "../styling/style.css";
 
-
 const buttonStyle: CSSProperties = {
-    width: "9.5rem",
-    height:  "1.8rem",
-    fontSize: "0.75rem",
-    backgroundColor: "white",
-    display: "flex",
-    alignItems: "center",
-    borderRadius: 0,
-    fontFamily: "Roboto",
-    textTransform: "capitalize",
-    margin: "1.8rem 6.25rem 0 0",  //Margin ska anpassas efter mediaqueries, återkommer till detta efter handledning med David
-    fontWeight: 500,
-}
+  width: "9.5rem",
+  height: "1.8rem",
+  fontSize: "0.75rem",
+  backgroundColor: "white",
+  display: "flex",
+  alignItems: "center",
+  borderRadius: 0,
+  fontFamily: "Roboto",
+  textTransform: "capitalize", //Margin ska anpassas efter mediaqueries, återkommer till detta efter handledning med David
+  fontWeight: 500,
+};
 
 const boxStyle: CSSProperties = {
-    width: "100%",
-    position: "fixed",
-    bottom: 0,
-    display: "flex",
-    justifyContent: "flex-end",
-    backgroundColor: theme.palette.primary.main
-}
+  width: "100%",
+  position: "fixed",
+  bottom: 0,
+  display: "flex",
+  justifyContent: "flex-end",
+  backgroundColor: theme.palette.primary.main,
+};
 
 export default function Footer() {
   return (
-    <div className= {"footerDiv"} style = {boxStyle}>
-      <Link style = {{textDecoration: "none"}} to={{ pathname: "/admin" }}>
-        <Button style = {buttonStyle}>Adminstratörssidan</Button>
+    <div className={"footerDiv"} style={boxStyle}>
+      <Link style={{ textDecoration: "none" }} to={{ pathname: "/admin" }}>
+        <StylesProvider injectFirst>
+          <Button style={buttonStyle}>Adminstratörssidan</Button>
+        </StylesProvider>
       </Link>
     </div>
   );
