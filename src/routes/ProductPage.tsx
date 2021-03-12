4eb7faee1f687bfe3d7b304342754789d39cca0e
mariahelenanoren <marialinder97@hotmail.com>
2021-03-12 11:13:19 +0100

    added dynamic routing for products

--- src/routes/ProductPage.tsx
+++ src/routes/ProductPage.tsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { products } from "./products";
 
-// interface Props {
-// }
+interface Product {
+  imageUrl: string;
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+}
 
-// interface State{
+interface Id {
+  id: string;
+}
 
-// }
+interface Props extends RouteComponentProps<Id> {}
 
-export default function ProductPage() {
+export default function ProductPage(props: Props) {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    products.map((item) => {
+      if (item.id === props.match.params.id) {
+        setProduct({
+          imageUrl: item.imageUrl,
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          description: item.description,
+        });
+      } else {
+      }
+    });
+  }, []);
+
   return <Link to={{ pathname: "/checkout" }}>Gå till checkoutpage</Link>;
 }
