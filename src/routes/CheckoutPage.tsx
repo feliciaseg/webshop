a80431a8fa44b2cd145f585c45618bbf8fdb7c06
mariahelenanoren <marialinder97@hotmail.com>
2021-03-16 12:03:20 +0100

    fixade responsivitet produktsida och footer checkout sida

--- src/routes/CheckoutPage.tsx
+++ src/routes/CheckoutPage.tsx
@@ -5,17 +5,24 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import PaymentForm from "../components/PaymentForm";
 
-
 export default function CheckoutPage() {
   return (
     <>
-    <Header type= "white"/>
-    <Box style = {{display: "flex", alignItems:"center", flexDirection: "column"}}>
-      <p>Betalning</p>
-  <PaymentForm/>
-  <p>Leveransmetod</p>
-  <DeliveryForm/>
-  </Box>
-  <Footer/>
-  </>);
+      <Header type="white" />
+      <Box
+        style={{
+          display: "flex",
+          flex: 1,
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <p>Betalning</p>
+        <PaymentForm />
+        <p>Leveransmetod</p>
+        <DeliveryForm />
+      </Box>
+      <Footer />
+    </>
+  );
 }
