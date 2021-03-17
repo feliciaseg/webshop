c077e853f182cb0843f1e22ca65fd692fae32156
mariahelenanoren <marialinder97@hotmail.com>
2021-03-17 11:54:29 +0100

    added button

--- src/routes/AdminPage.tsx
+++ src/routes/AdminPage.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import { products } from "./products";
 import ProductCardAdmin from "../components/ProductCardAdmin";
 import Grid from "../components/Grid";
 import { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import { CSSProperties } from "@material-ui/styles";
 
 interface Props {
   imageUrl: string;
@@ -43,9 +43,26 @@ export default function AdminPage() {
   return (
     <>
       <Header type={"white"} />
-      <div style={{ width: "100%", padding: "3.5rem" }}>
+      <div style={adminContainer}>
+        <Button style={addButton} variant="contained" color="primary">
+          Lägg till produkt
+        </Button>
         <Grid products={productList} />
       </div>
     </>
   );
 }
+
+const adminContainer: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  padding: "3.5rem",
+};
+const addButton: CSSProperties = {
+  width: "100%",
+  maxWidth: "20rem",
+  marginBottom: "2rem",
+  alignSelf: "flex-end",
+  borderRadius: 0,
+};
