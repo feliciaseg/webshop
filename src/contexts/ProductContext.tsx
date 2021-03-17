3029ef7004bc64092c7732afb21151170d577949
mariahelenanoren <marialinder97@hotmail.com>
2021-03-17 14:51:52 +0100

    added product context

---
+++ src/contexts/ProductContext.tsx
@@ -0,0 +1,58 @@
+import { Component, createContext } from "react";
+import { saveProductListToLocalStorage } from "../helper";
+
+interface Product {
+  imageUrl: string;
+  id: string;
+  name: string;
+  price: number;
+  description?: string;
+}
+
+interface ListState {
+  productList: Product[] | [];
+}
+interface ContextState extends ListState {
+  productList: Product[] | [];
+  addProduct: (product: Product) => void;
+  removeProduct: (product: Product) => void;
+}
+
+export const ProductContext = createContext<ContextState>({
+  productList: JSON.parse(localStorage.getItem("products") || "[]"),
+  addProduct: () => {},
+  removeProduct: () => {},
+});
+
+export default class ProductProvider extends Component<{}, ListState> {
+  state: ListState = {
+    productList: JSON.parse(localStorage.getItem("products") || "[]"),
+  };
+
+  addProduct = (product: Product) => {
+    this.setState({ productList: [...this.state.productList, product] });
+    saveProductListToLocalStorage(this.state.productList);
+  };
+
+  removeProduct = (product: Product) => {
+    const updatedList = this.state.productList.filter(
+      (item) => item.id !== product.id
+    );
+    this.setState({ productList: [...updatedList] });
+    saveProductListToLocalStorage(updatedList);
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          productList: this.state.productList,
+          addProduct: this.addProduct,
+          removeProduct: this.removeProduct,
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
